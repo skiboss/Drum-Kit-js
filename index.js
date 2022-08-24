e0b90aeda5f79a364749ae3a81075a7cfc297b22
Oghenemeru Avwemoya <skiboss4life@gmail.com>
2022-08-24 12:53:42 +0100

    Add: Key down event listener

--- index.js
+++ index.js
@@ -1,54 +1,61 @@
+// detecting buttonPress
 let numberOfDrums = document.querySelectorAll(".drum").length;
 
 for (let i = 0; i < numberOfDrums; i++) {
     document.querySelectorAll(".drum")[i].addEventListener("click", function (){
         var buttonInnerHTML = this.innerHTML;
-        switch (buttonInnerHTML) {
-            case "w":
-                var audio1 = new Audio("sounds/kick-bass.mp3")
-                audio1.play();
-                break;
 
-            case "a":
-                var audio2 = new Audio("sounds/crash.mp3")
-                audio2.play();
-                break;
+        makeSound(buttonInnerHTML);
+        
+    })
+};
 
-            case "s":
-                var audio3 = new Audio("sounds/tom-1.mp3")
-                audio3.play();
-                break;
+document.addEventListener("keydown", function(event) {
 
-            case "d":
-                var audio4 = new Audio("sounds/tom-2.mp3")
-                audio4.play();
+    makeSound(event.key);
+})
 
-            case "j":
-                var audio5 = new Audio("sounds/snare.mp3")
-                audio5.play();
-                break;
+function makeSound(key) {
+    switch (key) {
+        case "w":
+            var kickbass = new Audio("sounds/kick-bass.mp3")
+            kickbass.play();
+            break;
 
-            case "k":
-                var audio6 = new Audio("sounds/tom-3.mp3")
-                audio6.play();
-                break;
+        case "a":
+            var crash = new Audio("sounds/crash.mp3")
+            crash.play();
+            break;
 
-            case "l":
-                var audio7 = new Audio("sounds/tom-4.mp3")
-                audio7.play();
-                break;
+        case "s":
+            var tom1 = new Audio("sounds/tom-1.mp3")
+            tom1.play();
+            break;
 
-            default: console.log(buttonInnerHTML);
-                break;
+        case "d":
+            var tom2 = new Audio("sounds/tom-2.mp3")
+            tom2.play();
 
-        }
+        case "j":
+            var snare = new Audio("sounds/snare.mp3")
+            snare.play();
+            break;
 
-        document.addEventListener("keydown", function() {
-            alert("Key was Pressed!")
-        })
-})};
+        case "k":
+            var tom3 = new Audio("sounds/tom-3.mp3")
+            tom3.play();
+            break;
 
+        case "l":
+            var tom4 = new Audio("sounds/tom-4.mp3")
+            tom4.play();
+            break;
 
+        default: console.log(buttonInnerHTML);
+            break;
+    }
+
+}
 
 
 // Higher Order Function Calculator
