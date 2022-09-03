9ab5988a51ff94023f52e7855d5d6ff92ea1d609
Oghenemeru Avwemoya <skiboss4life@gmail.com>
2022-09-03 23:36:02 +0100

    Modify: Refactor JavaScript File

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-// detecting click
+// Detecting click
 let numberOfDrums = document.querySelectorAll(".drum").length;
 
 for (let i = 0; i < numberOfDrums; i++) {
@@ -9,7 +9,8 @@ for (let i = 0; i < numberOfDrums; i++) {
         buttonAnimation(buttonInnerHTML);
     })
 };
-// detecting buttonPress
+
+// Detecting buttonPress
 document.addEventListener("keydown", function(event) {
 
     makeSound(event.key);
@@ -17,44 +18,55 @@ document.addEventListener("keydown", function(event) {
 });
 
 function makeSound(key) {
+    var sound;
     switch (key) {
         case "w":
-            var kickbass = new Audio("sounds/kick-bass.mp3")
-            kickbass.play();
+            // var kickbass = new Audio("sounds/kick-bass.mp3")
+            // kickbass.play();
+            sound = new Audio("sounds/kick-bass.mp3")
             break;
 
         case "a":
-            var crash = new Audio("sounds/crash.mp3")
-            crash.play();
+            // var crash = new Audio("sounds/crash.mp3")
+            // crash.play();
+            sound = new Audio("sounds/crash.mp3")
             break;
 
         case "s":
-            var tom1 = new Audio("sounds/tom-1.mp3")
-            tom1.play();
+            // var tom1 = new Audio("sounds/tom-1.mp3")
+            // tom1.play();
+            sound = new Audio("sounds/tom-1.mp3")
             break;
 
         case "d":
-            var tom2 = new Audio("sounds/tom-2.mp3")
-            tom2.play();
+            // var tom2 = new Audio("sounds/tom-2.mp3")
+            // tom2.play();
+            sound = new Audio("sounds/tom-2.mp3")
+            break;
 
         case "j":
-            var snare = new Audio("sounds/snare.mp3")
-            snare.play();
+            // var snare = new Audio("sounds/snare.mp3")
+            // snare.play();
+            sound = new Audio("sounds/snare.mp3")
             break;
 
         case "k":
-            var tom3 = new Audio("sounds/tom-3.mp3")
-            tom3.play();
+            // var tom3 = new Audio("sounds/tom-3.mp3")
+            // tom3.play();
+            sound = new Audio("sounds/tom-3.mp3")
             break;
 
         case "l":
-            var tom4 = new Audio("sounds/tom-4.mp3")
-            tom4.play();
+            // var tom4 = new Audio("sounds/tom-4.mp3")
+            // tom4.play();
+            sound = new Audio("sounds/tom-4.mp3")
             break;
 
         default: console.log(buttonInnerHTML);
             break;
     }
+    sound.play();
+
 
 };
 
